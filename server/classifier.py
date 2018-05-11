f6275f0cfa9a079c4c1eb19dbc02b0035277d903
deqwin <294437759@qq.com>
2018-05-11 13:44:39 +0800

    feat: add classes

--- server/classifier.py
+++ server/classifier.py
@@ -21,6 +21,7 @@ model = None
 
 @csrf_exempt
 def classify(request):
+    print(request.FILES)
     img = request.FILES.get('image')
     img = Image.open(img)
     img = img.resize((224, 224))
@@ -30,18 +31,35 @@ def classify(request):
 
     global model
     if model is None:
-        model = DenseNet(classes=120, weights_path='server/dense161_result.h5')
+        model = DenseNet(classes=130, weights_path='server/dense161_result.h5')
 
     # 预测
-    preds = model.predict(x)
-    preds = preds.flatten()
-    preds = np.argmax(preds)
-
-    for key, classItem in map.items():
-        if classItem.get('id') == preds + 1:
-            classItem['image'] = 'image/'+key+'.jpg'
-            preds = classItem
+    y = model.predict(x)
+    y = y.flatten()
+    preds = []
+    max = (0, 0)
+    for index, item in enumerate(y):
+        if item > 0.5:
+            preds.append((index, item))
+        if max[1] <= item:
+            max = (index, item)
+        if len(preds) >= 3:
             break
 
-    info = json.dumps(preds, ensure_ascii=False)
+    if len(preds) == 0:
+        preds.append(max)
+
+    preds = sorted(preds, key=lambda x: x[1], reverse=True)
+    print(preds)
+
+    data = []
+    for predItem in preds:
+        for key, classItem in map.items():
+            if classItem.get('id') == predItem[0] + 1:
+                classItem['image'] = 'images/'+key+'.jpg'
+                classItem['similarity'] = predItem[1].item()
+                data.append(classItem)
+                break
+
+    info = json.dumps(data, ensure_ascii=False)
     return JsonResponse({"result": 0, "info": info})
